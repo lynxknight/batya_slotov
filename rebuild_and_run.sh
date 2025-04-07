68d04147507408c765ce00978ffa87a4c13a1500
Eduard Zhuk <zhuk@meta.com>
2025-04-07 19:42:01 +0100

    fix preferences and update rebuild

--- rebuild_and_run.sh
+++ rebuild_and_run.sh
@@ -1,3 +1,8 @@
 docker build -t tennisbooking -f build/Dockerfile .
-docker run -d -e TENNIS_USERNAME=$(cat .sensitive/.username) -e TENNIS_PASSWORD=$(cat .sensitive/.password) -e TENNIS_BOT_TOKEN=$(cat .sensitive/.telegram_bot_token) tennisbooking
+docker rm tennisbooking_container || true
+docker run -d --name tennisbooking_container \
+    -e TENNIS_USERNAME=$(cat .sensitive/.username) \
+    -e TENNIS_PASSWORD=$(cat .sensitive/.password) \
+    -e TENNIS_BOT_TOKEN=$(cat .sensitive/.telegram_bot_token) \
+    tennisbooking
 echo "Docker run status: $?"
