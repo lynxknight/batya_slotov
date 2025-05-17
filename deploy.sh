9ae4bbf331087ae0cd277793cf6b4f5d745929b0
Eduard Zhuk <zhuk@meta.com>
2025-05-17 16:56:15 +0100

    better

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-docker build -t tennisbooking -f build/Dockerfile .
-docker tag tennisbooking lynxknight/tennisbooking:latest
-docker push lynxknight/tennisbooking:latest && ssh 192.168.1.173 "/home/zhuk/pull_and_run_tennisbot.sh"
+docker build -t tennisbooking -f build/Dockerfile . && \
+docker tag tennisbooking lynxknight/tennisbooking:latest && \
+docker push lynxknight/tennisbooking:latest && \
+ssh 192.168.1.173 "/home/zhuk/pull_and_run_tennisbot.sh"
